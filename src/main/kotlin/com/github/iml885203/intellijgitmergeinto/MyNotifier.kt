96d75aefd4cc4feda193d24232e1a63785143366
Logan <iml885203@gmail.com>
2024-07-28 03:25:37 +0800

    ✨ notifyFailed add open git console action

--- src/main/kotlin/com/github/iml885203/intellijgitmergeinto/MyNotifier.kt
+++ src/main/kotlin/com/github/iml885203/intellijgitmergeinto/MyNotifier.kt
@@ -1,14 +1,24 @@
 package com.github.iml885203.intellijgitmergeinto
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationAction
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.vcs.console.ShowVcsConsoleTabAction
 
 object MyNotifier {
     fun notifyFailed(project: Project, content: String) {
-        NotificationGroupManager.getInstance()
+        val notificationGroup = NotificationGroupManager.getInstance()
             .getNotificationGroup("Git Merge Into")
+        val notification = notificationGroup
             .createNotification("Git merge into failed", content, NotificationType.WARNING)
-            .notify(project)
+        notification.addAction(object : NotificationAction("Open git console") {
+            override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                ShowVcsConsoleTabAction().actionPerformed(e)
+            }
+        })
+        notification.notify(project)
     }
 }
