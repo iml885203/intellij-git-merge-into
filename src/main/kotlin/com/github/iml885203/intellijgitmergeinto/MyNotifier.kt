1f28f1b0b5531dd8497f89ccee8da538f8589229
Logan <iml885203@gmail.com>
2024-11-15 17:56:15 +0800

    🐛 fix actionPerformed is OverrideOnly

--- src/main/kotlin/com/github/iml885203/intellijgitmergeinto/MyNotifier.kt
+++ src/main/kotlin/com/github/iml885203/intellijgitmergeinto/MyNotifier.kt
@@ -5,6 +5,7 @@ import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
 import com.intellij.vcs.console.ShowVcsConsoleTabAction
@@ -30,7 +31,8 @@ class MyNotifier(private var project: Project) {
                     templatePresentation.icon = AllIcons.Diff.ApplyNotConflicts
                 }
                 override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-                    GitResolveConflictsAction().actionPerformed(e)
+                    val action = ActionManager.getInstance().getAction("Git.ResolveConflicts")
+                    action?.actionPerformed(e)
                 }
             })
             .addAction(object : NotificationAction("Abort and back"){
@@ -51,7 +53,8 @@ class MyNotifier(private var project: Project) {
             .createNotification(title, content, type)
             .addAction(object : NotificationAction("Show detail") {
                 override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-                    ShowVcsConsoleTabAction().actionPerformed(e)
+                    val action = ActionManager.getInstance().getAction("Vcs.ShowConsoleTab")
+                    action?.actionPerformed(e)
                 }
             })
     }
